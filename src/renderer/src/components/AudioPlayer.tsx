d283a04437dcacec8fd7c4fe06d446416fd18874
bluven <yanshiyi1983@163.com>
2025-02-24 13:42:02 +0800

    Add a click event handling function for the spacebar

--- src/renderer/src/components/AudioPlayer.tsx
+++ src/renderer/src/components/AudioPlayer.tsx
@@ -1,32 +1,45 @@
-import { useEffect } from 'react'
-import { RiMenuAddLine } from 'react-icons/ri'
+import { useEffect } from 'react';
+import { RiMenuAddLine } from 'react-icons/ri';
 
-import { Controls } from './Controls'
-import { ProgressBar } from './ProgressBar'
-import { VolumeControl } from './VolumeControl'
-import { Title } from './Title'
-import Subtitles from './Subtitles'
-import FileSelect from './FileSelect'
-import { useAudioPlayerContext } from '@renderer/context/audio-player-context'
-
+import { Controls } from './Controls';
+import { ProgressBar } from './ProgressBar';
+import { VolumeControl } from './VolumeControl';
+import { Title } from './Title';
+import Subtitles from './Subtitles';
+import FileSelect from './FileSelect';
+import { useAudioPlayerContext } from '@renderer/context/audio-player-context';
 
 export const AudioPlayer = () => {
-  const { currentTrack, setCurrentTrackFromFilePath } = useAudioPlayerContext()
-  
+  const { currentTrack, setCurrentTrackFromFilePath, isPlaying, setIsPlaying } = useAudioPlayerContext();
+
   useEffect(() => {
     const handleAudioFileSelected = (event, filePath) => {
-      setCurrentTrackFromFilePath(filePath)
+      setCurrentTrackFromFilePath(filePath);
     };
     window.electron.ipcRenderer.on('audio-file-selected', handleAudioFileSelected);
     return () => {
-      window.electron.ipcRenderer.removeListener('audio-file-selected', handleAudioFileSelected)
-    }
-  }, [])
+      window.electron.ipcRenderer.removeListener('audio-file-selected', handleAudioFileSelected);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.code === 'Space') {
+        event.preventDefault();
+        setIsPlaying(!isPlaying);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isPlaying, setIsPlaying]);
 
   return (
     <div className="flex flex-col w-full h-full m-0 p-0 overflow-hidden">
       <Title />
-      {currentTrack == null ? <FileSelect />: <Subtitles /> }
+      {currentTrack == null ? <FileSelect /> : <Subtitles />}
       <div className="min-h-8 bg-[#2e2d2d] flex flex-col gap-9 lg:flex-row justify-between items-center text-white p-[0.5rem_10px] m-0">
         <div className="w-full flex flex-col items-center gap-1 m-auto flex-1">
           <Controls />
